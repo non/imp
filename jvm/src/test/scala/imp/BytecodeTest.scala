140ffe902fdbffa6d754e6b0d8d75e8681624313
Mike Curry <mikcurry@gmail.com>
2015-12-22 11:31:44 +0000

    Adds a couple of new tests for clarity, and updates test property descriptions

--- jvm/src/test/scala/imp/BytecodeTest.scala
+++ jvm/src/test/scala/imp/BytecodeTest.scala
@@ -6,27 +6,36 @@ class BytecodeTest extends PropSpec with Matchers {
 
   val b = Decompile.decompile("imp.Methods")
 
-  property("incr2 = incr1") {
+  property("Invocation on instance summoned with imp[A] is equivalent to direct invocation on an implicit parameter") {
     b("incr2") shouldBe b("incr1")
   }
 
-  property("incr3 = incr1") {
+  property("Invocation on instance created from an apply method implemented using macro summon, is equivalent to invocation on an implicit parameter") {
     b("incr3") shouldBe b("incr1")
   }
 
-  property("incr4 > incr1") {
+  property("Invocation on imp[A], implicit parameter or apply with macro summon is less bytecode than implicitly[A]") {
     b("incr4").length should be > b("incr1").length
   }
 
-  property("incr5 > incr1") {
+  property("Invocation on imp[A], implicit parameter, or apply with macro summon is less bytecode than normal apply pattern, even with @inline final") {
     b("incr5").length should be > b("incr1").length
   }
 
-  property("withImp = withExplicitParam") {
+  property("Apply implemented with macro summon is less bytecode than Apply implemented with imp[A]") {
+    b("incr6").length should be > b("incr3").length
+  }
+
+  property("Apply implemented with imp[A] generates the same amount of bytecode as normal apply pattern") {
+    b("incr6").length shouldBe b("incr5").length
+  }
+
+  property("Summoning with imp[A] inside method generates the same bytecode as an explicit parameter") {
     b("withImp") shouldBe b("withExplicitParam")
   }
 
-  property("withImplicitly > withImp") {
+  property("Summoning with imp[A] generates less bytecode than summoning with implicitly[A]") {
     b("withImplicitly").length should be > b("withImp").length
   }
+
 }
